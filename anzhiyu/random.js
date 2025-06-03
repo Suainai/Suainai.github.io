da3cae3fc8836d59f7a056cae45d26d2126d9eb6
Suainai <3278352909@qq.com>
2025-06-03 15:00:48 +0800

    Site updated: 2025-06-03 15:00:48

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,1 @@
-var posts=["2025/05/30/YunWei/","2025/05/27/hello-world/"];function toRandomPost(){
-    pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
-  };
+var posts=["2025/05/30/YunWei/","2025/05/27/hello-world/"];function toRandomPost(){pjax.loadUrl("/"+posts[Math.floor(Math.random()*posts.length)])}
