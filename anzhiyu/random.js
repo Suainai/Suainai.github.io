6a23ce47ccb9f08d70d3841a09bad5f1f125fea5
Suainai <3278352909@qq.com>
2025-06-05 11:25:52 +0800

    Site updated: 2025-06-05 11:25:52

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/05/30/YunWei/","2025/05/27/hello-world/","2025/06/04/Spring/"];function toRandomPost(){
+var posts=["2025/05/27/hello-world/","2025/06/04/Spring/","2025/05/30/YunWei/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
